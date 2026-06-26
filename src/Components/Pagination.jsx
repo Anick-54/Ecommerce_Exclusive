596430f5d922bf87b3b7731cd78e75f3e25f3c33
Anick <anisurrahmananick@gmail.com>
2026-06-27 01:27:08 +0600

    commit 1.17

--- src/Components/Pagination.jsx
+++ src/Components/Pagination.jsx
@@ -1,42 +1,48 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import ReactPaginate from 'react-paginate';
+import { Card4 } from './Card4';
 
-export const Pagination = ({ itemsPerPage, products }) => {
-  const items = products;
-
-  function Items({ currentItems }) {
-    return (
+export const Pagination = ({itemsParPage, item}) =>{
+  const items = item;
+  function Items({currentItems}){
+    return(
       <>
-        {currentItems &&
-          currentItems.map((item) => (
-            <div key={item.id}>
-              <h3>{item.title}</h3>
-            </div>
-          ))}
+      <div className='flex flex-wrap justify-center gap-6 mb-10'></div>
+      <Card4 key={item.id} item={item}/>
       </>
     );
   }
-  const [itemOffset, setItemOffset] = useState(0);
-  const endOffset = itemOffset + itemsPerPage;
-  const currentItems = items.slice(itemOffset, endOffset);
-  const pageCount = Math.ceil(items.length / itemsPerPage);
-
-  const handlePageClick = (event) => {
-    const newOffset = (event.selected * itemsPerPage) % items.length;
+  const[itemOffset, setItemOffset] = useState (0);
+  const endOffset = itemOffset + itemsParPage;
+  console.log(`Loading item from ${itemOffset} to ${endOffset}`);
+  const currentItems = items.slice(itemOffset,endOffset);
+  const pageCount = Math.ceil(items.lenght / itemsParPage);
+  const handlePageClick =(event) => {
+    const newOffset = (event.selected * itemsParPage) % items.lenght;
+    console.log(
+      `User requrested page number ${event.selected}, whice is offset ${newOffset}`
+    );
     setItemOffset(newOffset);
   };
   return (
     <>
       <Items currentItems={currentItems} />
       <ReactPaginate
-        breakLabel="..."
-        nextLabel="next >"
+        breakLabel='...'
+        nextLabel='next >'
         onPageChange={handlePageClick}
         pageRangeDisplayed={5}
         pageCount={pageCount}
-        previousLabel="< previous"
+        previousLabel='< previous'
         renderOnZeroPageCount={null}
+        className='flex gap-2.5'
+        pageClassName='px-6.25 py-0.5 bg-black text-white cursor-pointer'
       />
     </>
   );
-};
+
+  
+}
+
+
+  
--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,22 +1,24 @@
 import { useEffect, useState } from "react";
 import { BredCrumb } from "../Components/BredCrumb"
-import { Card4 } from "../Components/Card4";
 import { Container } from "../Components/Container"
 import { List } from "../Components/List"
 import { ListItem } from "../Components/ListItem"
 import { IoIosArrowForward } from "react-icons/io";
-import { Pagination } from "../Components/Pagination";
-import Item from "antd/es/list/Item";
+import { Card4 } from "../Components/Card4";
+// import { Pagination } from "../Components/Pagination";
 
 
 
 
 export const Shop = () => {
-    const [loading, setLoading] = useState (true)
 
+    // const [optionShow, setOptionShow] = useState (6)
+
+
+   const [loading, setLoading] = useState (true)
    const [products, setProducts] = useState ([])
 
-    useEffect ( () => {
+    useEffect (  () => {
         fetch('https://dummyjson.com/products')
         .then(res => res.json())
         .then( (data)=>{
@@ -33,6 +35,7 @@ export const Shop = () => {
             <div className="lg:flex">
                 <div className="w-[25%]! pl-12 lg:pl-0 h-50">
                     <List className="lg:mt-10 text-[16px] mt-4 lg:leading-9.5 w-58.25!">
+                        <ListItem className='text-[20px] font-poppins font-bold'>Shop By Catagory</ListItem>
                         <ListItem className="flex justify-between items-center">Woman's Fashion <IoIosArrowForward className="text-2xl" /></ListItem>
                         <ListItem className="flex justify-between items-center">Men's Fashion <IoIosArrowForward className="text-2xl" /></ListItem>
                         <ListItem>Electronics</ListItem>
@@ -47,33 +50,66 @@ export const Shop = () => {
                 <div className="w-[75%]">
                     <div className="flex justify-end gap-2 pt-10 lg:pt-0">
                         <h5 className="mt-1.5">Show :</h5>
-                        <select className="py-2 px-3 mb-5">
-                            <option value="US">6</option>
-                            <option value="CA">12</option>
-                            <option value="FR">18</option>
-                            <option value="DE">24 </option>
+                            <select className="py-2 px-3 mb-5">
+                            <option value={6}>6</option>
+                            <option value={12}>12</option>
+                            <option value={18}>18</option>
+                            <option value={24}>24</option>
                        </select>
                     </div>
                     <div className="lg:flex lg:flex-wrap pl-12 lg:pl-0 lg:justify-between">
                         {   
                             loading ?
-                            <div className="mx-auto w-full max-w-sm rounded-md border border-blue-300 p-4">
-                                <div className="flex animate-pulse space-x-4">
-                                    <div className="size-10 rounded-full bg-gray-200" />
-                                    <div className="flex-1 space-y-6 py-1">
-                                    <div className="h-4 rounded bg-gray-200" />
-                                    <div className="space-y-3">
-                                        <div className="grid grid-cols-3 gap-4">
-                                        <div className="col-span-2 h-4 rounded bg-gray-200" />
-                                        <div className="col-span-1 h-4 rounded bg-gray-200" />
+
+                            <div className="flex gap-0.5">
+                                    <div className="mx-auto w-full w-[300px]! min-h-[250px] rounded-md border border-blue-300 p-6">
+                                        <div className="flex animate-pulse space-x-4">
+                                            <div className="size-14 rounded-full bg-gray-200"></div>
+                                            <div className="flex-1 space-y-6 py-1">
+                                                <div className="h-3 rounded bg-gray-200"></div>
+                                                <div className="space-y-3">
+                                                    <div className="grid grid-cols-3 gap-4">
+                                                        <div className="col-span-2 h-3 rounded bg-gray-200"></div>
+                                                        <div className="col-span-1 h-3 rounded bg-gray-200"></div>
+                                                    </div>
+                                                    <div className="h-3 rounded bg-gray-200"></div>
+                                                </div>
+                                            </div>
                                         </div>
-                                        <div className="h-2 rounded bg-gray-200" />
                                     </div>
+                                    <div className="mx-auto w-full w-[300px]! min-h-[250px] rounded-md border border-blue-300 p-6">
+                                        <div className="flex animate-pulse space-x-4">
+                                            <div className="size-14 rounded-full bg-gray-200"></div>
+                                            <div className="flex-1 space-y-6 py-1">
+                                                <div className="h-3 rounded bg-gray-200"></div>
+                                                <div className="space-y-3">
+                                                    <div className="grid grid-cols-3 gap-4">
+                                                        <div className="col-span-2 h-3 rounded bg-gray-200"></div>
+                                                        <div className="col-span-1 h-3 rounded bg-gray-200"></div>
+                                                    </div>
+                                                    <div className="h-3 rounded bg-gray-200"></div>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
+                                    <div className="mx-auto w-full w-[300px]! min-h-[250px] rounded-md border border-blue-300 p-6">
+                                        <div className="flex animate-pulse space-x-4">
+                                            <div className="size-14 rounded-full bg-gray-200"></div>
+                                            <div className="flex-1 space-y-6 py-1">
+                                                <div className="h-3 rounded bg-gray-200"></div>
+                                                <div className="space-y-3">
+                                                    <div className="grid grid-cols-3 gap-4">
+                                                        <div className="col-span-2 h-3 rounded bg-gray-200"></div>
+                                                        <div className="col-span-1 h-3 rounded bg-gray-200"></div>
+                                                    </div>
+                                                    <div className="h-3 rounded bg-gray-200"></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                             </div>
                             :
-                            // <Pagination itemsPerPage={4} products={products}/>
+                            // <Pagination itemsPerPage={optionShow} products={products}/>
                             products.map((item) => (
                                 <Card4 key={item.id} item={item} />
                             ))
