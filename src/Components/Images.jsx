ad429d5c503a9ac5c4bbf54033b097a0b84f62f6
Anick <anisurrahmananick@gmail.com>
2026-07-10 23:32:04 +0600

    commit 1.29

--- src/Components/Images.jsx
+++ src/Components/Images.jsx
@@ -1,5 +1,4 @@
 
-
 export const Images = ({src}) => {
   return (
     <div>
--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,6 @@ import { SingUp } from './Pages/SingUp';
 
 
 
-
 const router = createBrowserRouter([
   {
     path: "/",
