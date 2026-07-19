f95b66e6c7654fc0a244699122d0c899f194a4f5
Anick <anisurrahmananick@gmail.com>
2026-07-19 23:52:09 +0600

    commit

--- src/Cart/CartSlice.js
+++ src/Cart/CartSlice.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import { createSlice } from '@reduxjs/toolkit'
 
-export const CartSlice = () => {
-  return (
-    <>
-    </>
-  )
+const initialState = {
+  value: 0,
 }
+
+export const Cartslice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    CartReducer: (state, action) => {
+      const exisitingItem = state.items.find((item) =>item.id ===action.payload.id);
+      if (exisitingItem) {
+        exisitingItem.quantity += 1;
+      }else{
+        state.items.push({...action.payload, quantity: 1});
+      }
+    },
+    
+  },
+})
+
+
+export const {  CartReducer } = Cartslice.actions
+
+export default Cartslice.reducer
--- src/store.jsx
+++ src/store.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import productReducer from './Slices/ProductSlice'
+import CartReducer from './Cart/CartSlice'
 
 export const store = configureStore({
   reducer: {
+    cart: CartReducer,
     allProducts: productReducer,
   },
 })
